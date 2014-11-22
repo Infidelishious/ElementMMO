df7a9343c0cdc00b0322b183570d3a127ec789a5
Uday Chadha <uday2794@gmail.com>
2014-11-22 15:03:39 -0800

    adding naked man and other tweaks

--- ElementMMO/core/src/com/imglow/ElementMMO/TextureSingleton.java
+++ ElementMMO/core/src/com/imglow/ElementMMO/TextureSingleton.java
@@ -12,7 +12,7 @@ import com.sun.media.jfxmedia.events.PlayerStateEvent.PlayerState;
 public class TextureSingleton {
 	private static TextureSingleton instance;
 	//////////////IF YOU TOUCH THIS CODE I WILL PEE ON YOUR MOM////////////////
-	public static int COP = 0, SPIKEY = 1, CHIKA = 2, OLD_MAN = 3, FAT_SAILOR = 4, NAKED_MAN = 5;
+	public static int COP = 0, SPIKEY = 1, CHIKA = 2, NAKED_MAN = 3;
 	public static int STAND = 0, FACE_UP = 1, FACE_LEFT = 2, WALK_DOWN = 3, WALK_UP = 4, WALK_LEFT_1 = 5,
 						WALK_LEFT_2 = 6, WALK_RIGHT_1 = 7, WALK_RIGHT_2 = 8, FACE_RIGHT = 9;
 	
@@ -21,8 +21,8 @@ public class TextureSingleton {
 	
 	public Texture white;
 	public BitmapFont scoreFont, nameFont;
-	public Texture cop, spikey;
-	public ArrayList<TextureRegion> copList, spikeyList;
+	public Texture cop, spikey, chika, nakedMan;
+	public ArrayList<TextureRegion> copList, spikeyList, chikaList, nakedManList;
 	public TextureRegion grass, bush, tree1, tree2;
 	
 	protected TextureSingleton(){}
@@ -43,9 +43,15 @@ public class TextureSingleton {
 		nameFont = new BitmapFont(Gdx.files.internal("fonts/FlxRegular.fnt"), Gdx.files.internal("fonts/FlxRegular_0.tga"), false);
 		cop = new Texture(Gdx.files.internal("images/cop.png"));
 		spikey = new Texture(Gdx.files.internal("images/Spikey.png"));
+		chika = new Texture(Gdx.files.internal("images/chika.png"));
+		nakedMan = new Texture(Gdx.files.internal("images/nake_man.png"));
+		nakedManList = new ArrayList<TextureRegion>();
 		copList = new ArrayList<TextureRegion>();
 		spikeyList = new ArrayList<TextureRegion>();
+		chikaList = new ArrayList<TextureRegion>();
 		playerSprites.add(copList);
+		playerSprites.add(spikeyList);
+		playerSprites.add(chikaList);	
 		Texture temp = new Texture(Gdx.files.internal("images/grass.png"));		//using to initialized all the green stuff
 		grass = new TextureRegion(temp);
 		temp = new Texture(Gdx.files.internal("images/bush.png"));
@@ -55,10 +61,19 @@ public class TextureSingleton {
 		temp = new Texture(Gdx.files.internal("images/tree2.png"));
 		tree2 = new TextureRegion(temp);
 		for(int i = 0; i < 9; i++) {
-			copList.add(new TextureRegion(cop, i * 16, 0, 16, 20));
+			copList.add(new TextureRegion(cop, i * 16, 0, 16, 21));
 		}
 		for(int i = 0; i < 9; i++) {
-			spikeyList.add(new TextureRegion(cop, i * 16, 0, 16, 21));
+			spikeyList.add(new TextureRegion(spikey, i * 16, 0, 16, 21));
+		}
+		for(int i = 0; i < 9; i++) {
+			chikaList.add(new TextureRegion(chika, i * 16, 0, 16, 21));
+		}
+		for(int i = 0; i < 9; i++) {
+			chikaList.add(new TextureRegion(chika, i * 16, 0, 16, 21));
+		}
+		for(int i = 0; i < 9; i++) {
+			nakedManList.add(new TextureRegion(nakedMan, i * 16, 0, 16, 21));
 		}
 	}
 }

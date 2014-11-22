f4a226015f119abb1b391bdee335adb3ba3c6d10
Ian Glow <ianglow@usc.edu>
2014-11-22 12:15:15 -0800

    Little Changes

--- ElementMMO/core/src/com/imglow/ElementMMO/Cell.java
+++ ElementMMO/core/src/com/imglow/ElementMMO/Cell.java
@@ -1,8 +1,12 @@
 package com.imglow.ElementMMO;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Cell implements Drawable {
+	
+	float x,y;
+	TextureRegion spr;
 
 	@Override
 	public void draw(SpriteBatch sb) {

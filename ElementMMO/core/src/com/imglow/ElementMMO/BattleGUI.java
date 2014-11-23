6923ade300410952cf74ad2bd6edf34227de6233
Liam Duffy <lduffy@usc.edu>
2014-11-22 16:29:11 -0800

    messing around with the battle GUI, trying to get a hello world

--- ElementMMO/core/src/com/imglow/ElementMMO/BattleGUI.java
+++ ElementMMO/core/src/com/imglow/ElementMMO/BattleGUI.java
@@ -1,5 +1,32 @@
 package com.imglow.ElementMMO;
 
-public class BattleGUI {
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.files.FileHandle;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+public class BattleGUI
+{
+//	SpriteBatch sb;
+	
+	public BattleGUI(/*SpriteBatch sb*/)
+	{
+//		this.sb = sb;
+	}
+	
+//	@Override
+//	public void draw(SpriteBatch sb) 
+//	{
+////		FileHandle f = Gdx.files.internal("/images/tree1.png");
+////		Texture t = new Texture(f);
+////		sb.draw(t, 10, 10);
+//	}
+	
+	public static void main(String args[])
+	{
+//		SpriteBatch sb = new SpriteBatch();
+//		BattleGUI bg = new BattleGUI(sb);
+//		bg.draw(sb);
+		
+	}
 }

3c9add7655f0a817de477249ad373f03fadec980
Liam Duffy <lduffy@usc.edu>
2014-11-22 16:37:05 -0800

    trying to do this lol

--- ElementMMO/core/src/com/imglow/ElementMMO/Battle.java
+++ ElementMMO/core/src/com/imglow/ElementMMO/Battle.java
@@ -4,6 +4,14 @@ public class Battle
 {
 	public Battle(CurrentPlayer currentPlayer, OtherPlayer otherPlayer)
 	{
+		//load the players information into the GUI.
 		
+		//client-event of choosing an element to use is sent to the server.
+		
+		//other player's message is taken from the server
+		
+		//update client's information based on other player's message
+		
+		//conditionals 
 	}
 }
--- ElementMMO/core/src/com/imglow/ElementMMO/BattleGUI.java
+++ ElementMMO/core/src/com/imglow/ElementMMO/BattleGUI.java
@@ -1,32 +1,24 @@
 package com.imglow.ElementMMO;
 
-//import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.files.FileHandle;
-//import com.badlogic.gdx.graphics.Texture;
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BattleGUI
+public class BattleGUI implements Drawable
 {
-//	SpriteBatch sb;
+	SpriteBatch sb;
 	
-	public BattleGUI(/*SpriteBatch sb*/)
+	public BattleGUI(SpriteBatch sb)
 	{
-//		this.sb = sb;
+		this.sb = sb;
 	}
 	
-//	@Override
-//	public void draw(SpriteBatch sb) 
-//	{
-////		FileHandle f = Gdx.files.internal("/images/tree1.png");
-////		Texture t = new Texture(f);
-////		sb.draw(t, 10, 10);
-//	}
-	
-	public static void main(String args[])
+	@Override
+	public void draw(SpriteBatch sb) 
 	{
-//		SpriteBatch sb = new SpriteBatch();
-//		BattleGUI bg = new BattleGUI(sb);
-//		bg.draw(sb);
-		
+		FileHandle f = Gdx.files.internal("/images/tree1.png");
+		Texture t = new Texture(f);
+		sb.draw(t, 10, 10);
 	}
 }

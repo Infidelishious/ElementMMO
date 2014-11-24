c818f923d975919e5ca0a2ce795b132281466608
Liam Duffy <lduffy@usc.edu>
2014-11-23 20:13:32 -0800

    added ability to close instruction pane to instruction pane

--- ElementMMO/core/src/com/imglow/ElementMMO/InstructionsPane.java
+++ ElementMMO/core/src/com/imglow/ElementMMO/InstructionsPane.java
@@ -16,8 +16,12 @@ public class InstructionsPane implements Drawable
 	@Override
 	public void draw(SpriteBatch sb) 
 	{
-		INSTRUCTION_FONT.draw(sb, "hello world", -150, 0);
-		sb.draw(TextureSingleton.getInstance().whiteRegion, -150, 0, 200, 200);
+		Button closeThisIsh = new Button(TextureSingleton.getInstance());
+		sb.draw(TextureSingleton.getInstance().whiteRegion, -200, 0, 400, 200);
+		INSTRUCTION_FONT.draw(sb, "Welcome to our game!", -190, 190);
+		INSTRUCTION_FONT.draw(sb, "Here's how to play:", -190, 170);
+		INSTRUCTION_FONT.draw(sb, "1. Run around and explore the world!", -190, 150);
+		INSTRUCTION_FONT.draw(sb, "2. Run around and explore the world!", -190, 150);
 	}
 	
 	public void close()
